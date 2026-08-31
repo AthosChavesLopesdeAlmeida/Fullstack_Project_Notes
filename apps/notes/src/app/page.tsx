261b42c489e647f0a921e20c5836b7f3fa5264a0
AthosChavesLopesdeAlmeida <athoschaveslopesdealmeida@gmail.com>
2026-08-31 20:56:22 -0300

    31/08/2026 - 20:56 - Created all of the landing page

--- apps/notes/src/app/page.tsx
+++ apps/notes/src/app/page.tsx
@@ -1,68 +1,83 @@
-import Image from "next/image";
+'use client'
+import { useRouter } from "next/navigation";
+
+import { ButtonGroup } from "@/components/ui/button-group";
+import { Button } from "@/components/ui/button";
+import { 
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription 
+} from "@/components/ui/card";
+
+import { Heading3, Feather, PaintbrushVertical, FeatherIcon } from "lucide-react";
 
 export default function Home() {
+  const router = useRouter()
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className="flex flex-col min-h-screen items-center">
+      <header className="w-full flex flex-row justify-between items-center px-20 py-6 bg-zinc-900">
+        <h1 className="font-bold text-4xl text-white">KeepInMind</h1>
+
+        <ButtonGroup>
+          <Button className="cursor-pointer w-26 h-9 bg-purple-400 hover:bg-purple-300" onClick={() => router.push('/register')}>Sign in</Button>
+          <Button className="cursor-pointer w-26 h-9" variant={"secondary"} onClick={() => router.push('/login')}>Log in</Button>
+        </ButtonGroup>
+      </header>
+
+      <main className="grid gird-col-1 gap-25 justify-items-center pt-20  w-8/10">
+        <section className="flex flex-col gap-10 items-center">
+          <h1 className="text-5xl font-bold text-center">Organize your thoughts in a lightweight and easy-to-use application</h1>
+
+          <h3 className="w-250 text-center">
+            KeepInMind is a notekeeping application that uses markdown to help you organize your thoughts, goals or just write anything you find useful.
+            KeepInMind is completely free of advertisements, thus giving you a better and cleaner experience. To use our services, create an account or log into yours 
+            if you already have one.
+          </h3>
+
+          <div className="flex flex-row gap-4">
+            <Button className="bg-purple-400 cursor-pointer hover:bg-purple-300  hover:-translate-x-1 hover:-translate-y-1" onClick={() => router.push('/register')}>
+              Create your account here
+            </Button>
+
+            <Button className=" cursor-pointer hover:-translate-x-1 hover:-translate-y-1" variant={"secondary"} onClick={() => router.push('/login')}>
+              Log into your account
+            </Button>
+          </div>
+        </section>
+
+        <section className="flex flex-row gap-6">
+
+          <Card className="w-80 hover:opacity-80 hover:-translate-x-1 hover:-translate-y-1 transition duration-300 ease-in-out">
+            <CardHeader>
+              <CardTitle><Heading3/> Support for markdown</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>KeepInMind is designed to display beautiful notes with markdown. You write in markdown and get nice results.</p>
+            </CardContent>
+          </Card>
+
+          <Card className="w-80 hover:opacity-80 hover:-translate-x-1 hover:-translate-y-1 transition duration-300 ease-in-out">
+            <CardHeader>
+              <CardTitle><PaintbrushVertical/>Amazing design</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>The application is bautifully designed to make you confortable and to free your mind while you create.</p>
+            </CardContent>
+          </Card>
+
+          <Card className="w-80 hover:opacity-80 hover:-translate-x-1 hover:-translate-y-1 transition duration-300 ease-in-out">
+            <CardHeader>
+              <CardTitle><FeatherIcon/>Lightweight</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>Your notes are light and the application does not consume much of your memory.</p>
+            </CardContent>
+          </Card>
+
+        </section>
       </main>
     </div>
   );
